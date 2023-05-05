809daac5b37515f221a397b5701db252b48f17cf
TrumpioDaiana <92599179+TrumpioDaiana@users.noreply.github.com>
2023-05-05 01:09:25 -0300

    create react file

---
+++ app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor() {
+        super()
+    }
+
+    {/* Javascript functions can be written here */}
+
+    render() {
+        return (
+            <div className="App">
+            Hello World!
+            </div>
+        )
+    }
+}
+
+export default App
